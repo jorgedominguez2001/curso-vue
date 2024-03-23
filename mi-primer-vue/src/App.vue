aa015c7d73ba0a225b37fb63d0d9e3f4931308d2
jorge <jorgeivands165@gmail.com>
2024-03-22 19:09:06 -0600

    initial commit (mi-primer-vue): :tada: first commit

---
+++ mi-primer-vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+const name = "jorge dominguez";
+const stylecolor = "color: blue";
+const arraycolores = ["purple","orange","yellow"];
+const activo =  true;
+</script>
+
+<template>
+  <h1 >Hola {{ name }}</h1>
+  <h2>{{ arraycolores }}</h2>
+  <h2 :style="`color: ${arraycolores[2]}`">soy azul</h2>
+</template>
+<style>
+h1{
+  color: red;
+}
+</style>
