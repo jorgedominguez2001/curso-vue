77033615c8a2b54c971509feb02d630890bf5481
jorge <jorgeivands165@gmail.com>
2024-03-27 16:18:35 -0600

    general updated (mi-primer-vue): :zap: add add button and restriction on repeating numbers

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,34 +1,64 @@
-<script setup>
-import {ref} from 'vue'
-let counter =ref(0);
-const incremento = () => {
-    counter.value ++;
-};
-const disminucion = () => {
-    counter.value --;
-};
-const resetear = () => {
-    counter.value =(0);
-};
-</script>
-
 <template>
-    <h2 :class="counter >= 0 ? 'positive' : 'negative'">
-        {{ counter }}
-    </h2>
-    <button @click="incremento">aumentar</button>
-    <button @click="disminucion">disminuir</button>
-    <button @click="resetear">resetear</button>
+  <h2 :class="classCounter">
+    {{ counter }}
+  </h2>
+  <button @click="incremento">aumentar</button>
+  <button @click="disminucion">disminuir</button>
+  <button @click="resetear">resetear</button>
+  <button @click="añadir" :disabled="blockNumber">Add</button>
+  <ul>
+    <li v-for="(numero, index) in array" :key="index">{{ numero }}</li>
+  </ul>
 </template>
 
+<script setup>
+import { ref, computed } from 'vue';
+
+let counter = ref(0);
+let array = ref([]);
+
+const incremento = () => {
+  counter.value++;
+};
+
+const disminucion = () => {
+  counter.value--;
+};
+
+const resetear = () => {
+  counter.value = 0;
+};
+
+const añadir = () => {
+  const numsearch = array.value.find(numero => numero === counter.value);
+  if (numsearch === undefined) {
+    array.value.push(counter.value);
+  }
+};
+
+const classCounter = computed(() => {
+  if (counter.value === 0) {
+    return "zero";
+  } else if (counter.value > 0) {
+    return "positive";
+  } else {
+    return "negative";
+  }
+});
+
+const blockNumber = computed(() => {
+  return array.value.includes(counter.value);
+});
+</script>
+
 <style>
-h1{
-  color: red;
-}
-.positive{
+.positive {
   color: green;
 }
-.negative{
+.negative {
   color: red;
+}
+.zero {
+  color: blueviolet;
 }
 </style>
